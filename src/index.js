3a51355ba65ef68af5135c7c65f70ab5638a2391
Slavoljub Popovic <18499909+slavo3dev@users.noreply.github.com>
2019-11-18 17:01:09 -0500

    adding

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from "react";
 import { render } from "react-dom";
 import Videojs from "./video.js";
 
+import "./styles.css";
+
+// const src = "https://kanapi.media.kan.org.il/Redirector/ipbc/direct/kan-reka/hls-live.m3u8";
+
+const src =
+  "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8";
+
 const videoJsOptions = {
   autoplay: false,
   playbackRates: [0.5, 1, 1.25, 1.5, 2],
-  width: 720,
-  height: 300,
+  width: 900,
+  height: 500,
   controls: true,
   sources: [
     {
-      src:
-        "https://kanapi.media.kan.org.il/Redirector/ipbc/direct/kan-reka/hls-live.m3u8",
+      src: src,
       type: "application/x-mpegURL"
     }
   ]
